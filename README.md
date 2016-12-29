f304069db4aa59a0894a8ff3a190da4258b0dbc3
hefei <hefei@zhugeio.com>
2016-12-29 15:37:13 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # css2sass
 convert css file to sass file
+## css2sass is a easy way to convert css file to sass file. For example, it can convert
+    .container {
+        display: block;
+        width: 100px;
+        height: 100px;   
+    }
+to
+    .container
+      display: block
+      width: 100px
+      height: 100px
+      
---
+++ css2sass.js
@@ -0,0 +1,59 @@
+var fs = require('fs')
+var path = require('path')
+
+var regExp = {
+  rule: /.+{(\n*|.*)*?}/g,
+  statement: /([^;{}]*;)+?/g,
+  comments: /\/\*([^\*\/])*\*\//g
+}
+
+function css2sass(sourcePath, targetPath) {
+  var source = fs.statSync(sourcePath)
+  if(source.isFile()){
+    if(!targetPath){
+      targetPath = sourcePath.replace('.css', '.sass')
+    }
+  }else {
+    throw new Error('sourcePath is not exist')
+  }
+  
+  var buffer = fs.readFileSync(sourcePath)
+  var ruleList = matchs(regExp.rule, buffer.toString())
+  var resultList = ruleList.map( rule => convert(rule))
+  var result = resultList.join('\n')
+  fs.writeFileSync(targetPath, result)
+}
+
+function convert(rule) {
+  var result = ''
+  var selector = /[^{]*/.exec(rule)
+  if (selector) {
+    result += selector[0] + '\n'
+  }
+  var statements = matchs(regExp.statement, rule)
+  var statements = statements.map(statement => {
+    statement = statement.replace(';', '').replace(/ /g, '').replace(/\n/g, '')
+    var arr = statement.split('*/')
+    if (arr.length > 1) {
+      arr[0] += '*/'
+      arr[1] = '  ' + arr[1]
+    }
+    statement = arr.join('\n')
+    if (statement.indexOf('\n') === -1) {
+      statement = statement + '\n'
+    }
+    return '  ' + statement
+  })
+  return result + statements.join('')
+}
+
+function matchs(reg, str) {
+  var result = []
+  var match
+  while (match = reg.exec(str)) {
+    result.push(match[0])
+  }
+  return result
+}
+
+module.exports = css2sass
